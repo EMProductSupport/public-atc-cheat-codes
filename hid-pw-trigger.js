8581730278387d427482602551d0be4c35794790
EM Product Support <65433041+EMProductSupport@users.noreply.github.com>
2020-05-16 13:57:05 -0500

    Create hid-pw-trigger.js

---
+++ hid-pw-trigger.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('div[data-price-widget="5eb87a01cf367910ae4e023b"], div[data-price-widget="5eb87b7ccf367910bc194502"], div[data-price-widget="5eb87d27cf367910ae4e0247"], div[data-price-widget="5eb87e9bcf367910ae4e024c"]').css("display", "none");
+});
